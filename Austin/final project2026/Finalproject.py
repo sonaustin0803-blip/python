80514957ef44e08367cf1cad232626e800216766
sonaustin0803-blip <sonaustin0803@gmail.com>
2026-06-05 21:10:54 +0800

    Add files via upload

---
+++ Austin/final project2026/Finalproject.py
@@ -0,0 +1,108 @@
+from pycat.core import Window,Sprite,Scheduler,KeyCode,Color,Player,Label
+import random
+window = Window(background_image='background .jpg')
+
+game_muice_1= Player('game muice 1.mp3')
+game_muice_2= Player('game muice 2.mp3')
+game_muice_3= Player('game muice 3.mp3')
+game_muice_4= Player('game muice 4.mp3')
+money= Player('money.mp3')
+class Player(Sprite):
+
+    def on_create(self):
+        self.image="pubg.png"
+        self.scale = 0.05
+        self.x=640
+        self.y=320
+        self.speed = 10
+        self.position = window.center
+        self.add_tag("player")
+        self.score = 0
+
+    def on_update(self,dt):
+        if window.is_key_pressed(KeyCode.W):
+            self.y += 10
+        if window.is_key_pressed(KeyCode.A):
+            self.x -= 10
+        if window.is_key_pressed(KeyCode.S):
+            self.y -= 10
+        if window.is_key_pressed(KeyCode.D):
+            self.x += 10
+        if window.is_key_pressed(KeyCode. UP):
+            self.y += 10
+        if window.is_key_pressed(KeyCode.LEFT):
+            self.x -= 10
+        if window.is_key_pressed(KeyCode.DOWN):
+            self.y -= 10
+        if window.is_key_pressed(KeyCode.RIGHT):
+            self.x += 10
+
+    def on_left_click_anywhere(self):
+        window.create_sprite(bullet)
+
+class bullet(Sprite):
+    def on_create(self):
+        self.image='7785269-middle..png'
+        self.position = player.position
+        self.speed = 10
+        self.scale = 0.06
+        self.point_toward_mouse_cursor()
+        self.add_tag("bullet")
+    def on_update(self, dt):
+        self.move_forward(45)
+        if self.is_touching_window_edge():
+            self.delete()
+
+class Enemy(Sprite):            
+
+    def on_create(self):
+        self.image='images (66).jpg'
+        self.scale = 0.2
+        self.speed = 5
+        self.goto_random_position()
+        self.rotation = random.randint(0,360)
+        self.time = 0
+        self.bullet_time = 0.05
+
+    def on_update(self, dt):
+        self.move_forward(self.speed)
+        self.time += dt
+        if self.time > self.bullet_time:
+            enemy_bullet = window.create_sprite(Enemybullet)
+            enemy_bullet.position = self.position
+            enemy_bullet.point_toward_sprite(player)
+            self.time = 0 
+        
+        if self.is_touching_window_edge():
+            self.delete()
+        if self.is_touching_any_sprite_with_tag("bullet"):
+            money.play()
+            player.score +=10000
+            score_label.text = 'get USdollar how many = ' + str(player.score)
+            self.delete()
+
+            self.delete()
+
+class Enemybullet(Sprite):
+    def on_create(self):
+        self.image='Usdollar.jpg'
+        self.speed = 10
+        self.scale = 0.06
+        self.add_tag("enemybullet")
+    def on_update(self, dt):
+        self.move_forward(self.speed)
+        if self.is_touching_window_edge():
+            self.delete()
+        if self.is_touching_any_sprite_with_tag("player"):
+            player.score -=50
+            score_label.text = 'get USdollar how many = ' + str(player.score)
+            self.delete()
+
+def spawn_enemy():
+    window.create_sprite(Enemy)
+
+Scheduler.update(spawn_enemy, delay=1)
+player = window.create_sprite(Player)
+score_label = window.create_label(text='get USdollar how many = '+ str(player.score), x = 10 , y = 640)
+
+window.run()
